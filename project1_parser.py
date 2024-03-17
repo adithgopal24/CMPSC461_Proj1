970b435ab1862068a357b2645b2ec9225d01f149
adithgopal24 <azg5941@psu.edu>
2024-03-16 21:20:35 -0400

    Attempting to parse token character-by-character. Additionally, attempting to determine what an expression is, and handle multiple expressions in input.

--- project1_parser.py
+++ project1_parser.py
@@ -2,51 +2,53 @@
 class Lexer:
     def __init__(self, code):
         self.code = code.strip().replace("\n", " ").replace("\t", "")
-        self.code += "\0"
+        #self.code += "dummy"
         self.position = 0
 
     # move the lexer position and identify next possible tokens.
     def get_token(self): #keep adding values until you reach =, or operator and (), then return that
         tokens = list(self.code)
-        print("len of tokens: ", len(tokens))
-        print("tokens list", tokens)
+        #print("len of tokens: ", len(tokens))
+        #print("tokens list", tokens)
+        #print(len(self.code) - 1)
         # tokens = self.code.replace("\n", "")
         string = ""
         non_token = ["(", ")", "=", "+", "/", "*", "-", " ", "<", ">"]
         arith = ["+", "/", "*", "-"]
+        #print(tokens[self.position])
+        ''''''
+        #print(tokens[self.position])
+        while self.position < (len(self.code)):
+            #print("goes into while loop")
+            if tokens[self.position] == " ":
+                self.position += 1
+                #print("goes in while loop that skips spaces")
 
-
-        while (tokens[self.position] == " ") and (self.position < (len(self.code)-1)): #while loop that skips spaces
-            print("space")
-            self.position += 1
-        print("not space")
-
-        if tokens[self.position] not in non_token:
-            while (tokens[self.position] not in non_token) and (self.position < (len(self.code)-1)):
+            elif tokens[self.position] not in non_token:
+                #while (tokens[self.position] not in non_token) and (self.position < (len(self.code)-1)):
                 string += tokens[self.position]
                 self.position += 1
-                #return string
-        else:
-            if tokens[self.position] in arith:
-                string += tokens[self.position]
-                self.position += 1
-            elif tokens[self.position] == "=":
-                if tokens[self.position + 1] == "=":
-                    string += tokens[self.position + 1] + tokens[self.position]
-                    self.position += 2
-                else:
+                    #return string
+            else:
+                if tokens[self.position] in arith:
                     string += tokens[self.position]
                     self.position += 1
-            # elif tokens[self.position] == " ":
-            #     self.position += 1
-            if string == "":
-                self.position += 1
-            # else:
-            #     break
+                if tokens[self.position] == "=":
+                    if tokens[self.position + 1] == "=":
+                        string += tokens[self.position + 1] + tokens[self.position]
+                        self.position += 2
+                    else:
+                        string += tokens[self.position]
+                        self.position += 1
+                # elif tokens[self.position] == " ":
+                #     self.position += 1
+                if string == "":
+                    self.position += 1
 
-        if string.isdigit():
-            string = int(string)
-            print("yo mama")
+            if string.isdigit():
+                string = int(string)
+                print("is digit")
+            #self.position += 1
         return string
 
         #print(tokens)
@@ -73,6 +75,7 @@ class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = None
+        self.expression_counter = 0
 
     # function to parse the entire program, expected output
     def parse(self):
@@ -86,6 +89,8 @@ class Parser:
 
     # move to the next token.
     def advance(self):
+        self.current_token = self.lexer.get_token()
+        '''
         if self.lexer.position < len(self.lexer.code):
             self.current_token = self.lexer.get_token()
         else:
@@ -94,10 +99,10 @@ class Parser:
         print(len(self.lexer.code))
         #return self.current_token
         #lexer.position += 1
-
+        '''
     # parse the one or multiple statements
     def program(self): #calls other cases based on first token
-        self.advance()
+        #self.advance()
         if self.current_token == "if":
             return self.if_statement()
         elif self.current_token == "while":
@@ -113,6 +118,7 @@ class Parser:
     # parse assignment statements,expressions
     def assignment(self): #('=', 'x', ('+', 5, 3))
         token_1 = self.current_token
+        print(self.current_token)
         self.advance()
         token_2 = self.current_token #equals sign
         self.advance()
@@ -123,20 +129,49 @@ class Parser:
     # parse arithmetic experssions
     def arithmetic_expression(self):
         arith = ["+", "-", "*", "/"]
+        all_symbols = ["+", "-", "*", "/", "="]
         val = ""
         token_1 = ""
         token_2 = ""
         token_3 = ""
+        arith_token_1 = ""
+        arith_token_2 = ""
+        token_counter = 0
         while self.lexer.position < (len(self.lexer.code)-1): #run until end of expression, not end of tokens. Curr_token = digit, next_token = letter?
-            #print("17")
-            if self.current_token not in arith:
-                token_1 = self.current_token
-                self.advance()
-                token_2 = self.current_token
-                self.advance()
-                token_3 = self.current_token
-                self.advance()
-        return token_2, token_1, token_3
+            #check for space, tok_before and after is numOrDigit,
+            if self.lexer.code[self.lexer.position] == " ":
+                if (self.lexer.code[self.lexer.position - 1]) and (self.lexer.code[self.lexer.position + 1]) not in all_symbols: #this represents a new expression
+                    self.expression_counter += 1
+            self.lexer.position += 1
+
+
+        if self.expression_counter <= 1: #only one or less expressions in input
+            while token_counter < (len(self.lexer.code)-1):
+                if self.current_token not in arith:
+                    if self.current_token == " ": #account for "space" tokens
+                        self.advance()
+                    else:
+                        token_1 = self.current_token #arith symbol
+                        self.advance()
+                        #token_2 = self.current_token #first value in arith exp
+                        #self.advance()
+                        token_3 = self.current_token #second value in arith exp
+                        self.advance()
+                else: #if token is arithmetic symbol
+                    token_2 = self.current_token
+                    self.advance()
+                token_counter += 1
+            return token_2, token_1, token_3
+
+
+        else: #multiple arith expressions in input
+            if self.lexer.code[self.lexer.position] == " ":
+                if (self.lexer.code[self.lexer.position - 1]) and (self.lexer.code[self.lexer.position + 1]) not in all_symbols:
+                    arith_token_1 = self.lexer.code[self.lexer.position + 1]
+
+
+
+
 
     def term(self):
         pass
@@ -160,17 +195,19 @@ class Parser:
 
 # Testing
 lexer = Lexer('x = 5 + 3')
+#lexer = Lexer('x = 1 y = 2 z = 3 a = x + y + z')
 
-#lexer = Lexer(code_2)
 parser = Parser(lexer).parse()
+#print(lexer.get_token())
+#print(parser)
+'''
 print(lexer.get_token())
 print(lexer.get_token())
 print(lexer.get_token())
 print(lexer.get_token())
-print(lexer.get_token())
+'''
 #t = (lexer.get_token(), lexer.get_token(), lexer.get_token(), lexer.get_token(), lexer.get_token(), lexer.get_token(), lexer.get_token())
 #print(t)
-print(parser)
 #print(code_2.strip())
 '''
 
